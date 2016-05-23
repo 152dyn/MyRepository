c743ebf0fc19551f8ced8689b7637fdcc2b94f2e
152dyn <823150076@qq.com>
2016-05-23 20:11:40 +0800

    效果更新

--- 虎门国际购物中心（部分）/js/food-amuse.js
+++ 虎门国际购物中心（部分）/js/food-amuse.js
@@ -181,6 +181,57 @@ function actSide() {
 	$('.conright, .conleft').slideDown(2000);
 }
 actSide();
+/*	
+ * 功能：活动纵览：大图滚动
+ * 参数：boxWidth  外边框宽度
+ *		 conWidth  内边框宽度
+ *		 conliWidth 内容小块宽度
+ *		 conliLen   内容长度
+ * 		 step       速度值
+ *		 change     判断方向
+ *		 boxScorl   滚动距离
+ * 作者：北有喬木
+ */
+ function actBrow() {
+ 	var boxWidth = 0;
+ 	var conWidh = 0;
+ 	var conliWidth = 0;
+ 	var conliLen = 0;
+ 	var step = 2;
+ 	var change = 1;
+ 	var boxScorl = 0;
+ 	var timer = setInterval(browMove,100);
+ 	boxWidth = $('.brow-con').width();
+ 	conliLen = $('.brow-con ul li').length;
+ 	conliWidth = $('.brow-con ul li').outerWidth();
+ 	$('.brow-con ul').outerWidth(conliLen * conliWidth + 120);
+ 	conWidh = $('.brow-con ul').outerWidth();
+ 	boxScorl = $('.brow-con').scrollLeft();
+ 	$('.brow-left').click(function() {
+ 		change = 1;
+ 		browMove();
+ 	})
+ 	$('.brow-right').click(function() {
+ 		change = -1;
+ 		browMove();
+ 	})
+ 	function browMove() {
+ 		if (change > 0) {
+ 			boxScorl += step;
+ 			if (boxScorl >= conWidh - boxWidth) {
+ 				boxScorl = 0;
+ 			};
+ 		}
+ 		if (change < 0) {
+ 			boxScorl -= step;
+ 			if (boxScorl <= 0) {
+ 				boxScorl = conWidh - boxWidth;
+ 			};
+ 		};
+ 		$('.brow-con').scrollLeft(boxScorl);
+ 	}
+ }
+ actBrow();
 /*
  * 功能：活动推广场地：滑动效果、自定义滚动条
  * 参数：oldX 鼠标初始点击值
@@ -194,15 +245,20 @@ actSide();
  	var liLen = 0;
  	var liWidth = 0;
  	var oldX = 0;
+ 	var oldY = 0;
  	var nowDisX = 0;
+ 	var nowDisY = 0;
  	var newX = 0;
+ 	var newY = 0;
+
  	liLen = $('.ic-con ul li').length;
  	liWidth = $('.ic-con ul li').outerWidth();
+ 	quotHeight = $('.quoted').outerHeight();
+ 	newsHeight = $('.newscon').outerHeight();
  	$('.ic-con ul').width(liLen * liWidth);
  	$('.coop-scroll').mousedown(function(e) {
  		oldX = e.clientX;
 		nowDisX = $('.coop-scroll').offset().left - $('.coop-scrbox').offset().left;
-		console.log(nowDisX);
 		e.preventDefault();
 		$(document).mousemove(function(e) {
 			e.preventDefault();
@@ -221,8 +277,56 @@ actSide();
 			})
 		})
  	})
+ 	$('.newscrol').mousedown(function(e) {
+ 		oldY = e.clientY;
+		nowDisY = $('.newscrol').offset().top - $('.newscrbox').offset().top;
+		e.preventDefault();
+		$(document).mousemove(function(e) {
+			e.preventDefault();
+			newY = e.clientY;
+			var nowY = nowDisY + newY - oldY;
+			var maxY = $('.newscrbox').outerHeight() - $('.newscrol').outerHeight(); 
+			if (nowY <= 0) {
+				nowY = 0;
+			};
+			if (nowY >= maxY) {
+				nowY = maxY;
+			};
+			$('.newcons').scrollTop(nowY / maxY * ($('.newcons').innerHeight()));
+			$('.newscrol').css({
+				"top": nowY + 'px'
+			})
+		})
+ 	})
  	$(document).mouseup(function () {
 		$(document).unbind("mousemove");
 	})
  }
  coopMove();
+ /*
+ * 功能：活动与推广（最新动态）：图片渐变效果
+ * 参数：imgIndex 初始索引值
+ * 作者：北有喬木
+ */	
+ function proFade() {
+ 	var imgIndex = 0;
+ 	var timer = setInterval(proMove,2000);
+ 	function proMove() {
+ 		$('.acti-img img').eq(imgIndex).fadeIn(2000).siblings('img').fadeOut(2000);
+ 		$('.acti-img p span').eq(imgIndex).addClass('act-s').siblings().removeClass('act-s');
+ 		imgIndex ++;
+ 		if (imgIndex == $('.acti-img img').length) {
+ 			imgIndex = 0;
+ 		};
+ 	}
+ 	$('.acti-img p span').click(function() {
+ 		if (timer) {
+ 			clearInterval(timer);
+ 		};
+ 		imgIndex = $(this).index();
+ 		$('.acti-img img').eq(imgIndex).fadeIn(2000).siblings('img').fadeOut(2000);
+ 		$('.acti-img p span').eq(imgIndex).addClass('act-s').siblings().removeClass('act-s');
+ 		timer = setInterval(proMove,2000);
+ 	}) 
+ }
+ proFade();
